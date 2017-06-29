e92942b5a4e9d11965bfe18636a03d77bcca38e8
Jeff Gong <jgong@berkeley.edu>
2017-06-29 14:36:34 +0800

    Fix subreddit score threshold with private module class

--- lib/scraper/scraper.rb
+++ lib/scraper/scraper.rb
@@ -1,5 +1,19 @@
 module Scraper
 
+	class ScoreLinter
+		def return_subreddit_score(score)
+			if score.end_with?('k')
+				# Score is above the ten thousand threshold
+				score = score.slice(0, score.length - 1)
+				score_thousands, score_hundreds = score.split('.')
+				total_score = score_thousands.to_i * 1000 + score_hundreds.to_i * 100
+				return total_score
+			else
+				return score.to_i
+			end
+		end
+	end
+
 	def self.retrieve_entries(subreddit)
 		return Entry.where(subreddit: subreddit)
 	end
@@ -15,6 +29,7 @@ module Scraper
 
 	def self.scrape_subreddit(subreddit="all")
 		require 'open-uri'
+		linter = ScoreLinter.new
 		doc = Nokogiri::HTML(open("https://www.reddit.com/r/" + subreddit + "/", 'User-Agent' => 'Tempest'))
 
 		entries = doc.css('.thing')
@@ -22,7 +37,7 @@ module Scraper
 		entries.each do |entry|
 			title = entry.css('p.title>a').text
       		link = entry.css('p.title>a')[0]['href']
-      		score = entry.css('.score')[1].text.to_i
+      		score = linter.return_subreddit_score(entry.css('.score')[1].text)
       		completeEntry = Entry.create(subreddit: subreddit, title: title, link: link, score: score)
       	end
 	end
