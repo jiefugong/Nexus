af7a8601aa196f3d2871b05f3668f52a8e16dde6
Jeff Gong <jgong@berkeley.edu>
2017-08-17 15:31:24 -0700

    Remove extraneous copy of subreddit_button.jsx

--- app/assets/javascripts/components/subreddit_button.jsx
+++ app/assets/javascripts/components/subreddit_button.jsx
@@ -2,10 +2,12 @@ class SubredditButton extends React.Component {
 
 	render() {
 		let buttonClass = "btn btn-default btn-warning";
-		return <input
-				className={this.props.buttonActive ? buttonClass.concat(" active") : buttonClass}
-				type="button"
-				value={this.props.subreddit}
-				onClick={this.props.onClick}/>
+		return (
+			<input
+			  className={this.props.buttonActive ? buttonClass.concat(" active") : buttonClass}
+			  type="button"
+			  value={this.props.subreddit}
+			  onClick={this.props.onClick}/>
+		);
 	}
 }
